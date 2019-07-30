e97a667dd0c4dca6702c4d92b5dd4982c5df3b00
Luhurhidayat <45382134+Luhurhidayat@users.noreply.github.com>
2019-07-30 13:03:27 +0700

    Update insert.php

--- www/insert.php
+++ www/insert.php
@@ -2,11 +2,12 @@
  include "db.php";
  if(isset($_POST['insert']))
  {
+ $nama_pembeli=$_POST['nama_pembeli'];
  $nama_barang=$_POST['nama_barang'];
  $unit=$_POST['unit'];
  $harga=$_POST['harga'];
  $jumlah=$_POST['jumlah'];
- $q=mysqli_query($con,"INSERT INTO `pembelian` (`nama_barang`,`unit`,`harga`,`jumlah`) VALUES ('$nama_barang','$unit','$harga','$jumlah')");
+ $q=mysqli_query($con,"INSERT INTO `data` (`nama_pembeli`,`nama_barang`,`unit`,`harga`,`jumlah`) VALUES ('$nama_pembeli','$nama_barang','$unit','$harga','$jumlah')");
  if($q)
   echo "success";
  else
