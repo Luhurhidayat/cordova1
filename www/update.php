3afd76b83d9d43bc7fb8e7981251474007ba729d
Luhurhidayat <45382134+Luhurhidayat@users.noreply.github.com>
2019-07-30 13:05:18 +0700

    Update update.php

--- www/update.php
+++ www/update.php
@@ -3,11 +3,12 @@
  if(isset($_POST['update']))
  {
  $Id=$_POST['Id'];
+  $nama_pembeli=$_POST['nama_pembeli'];
  $nama_barang=$_POST['nama_barang'];
  $unit=$_POST['unit'];
  $harga=$_POST['harga'];
  $jumlah=$_POST['jumlah'];
- $q=mysqli_query($con,"UPDATE `pembelian` SET `nama_barang`='$nama_barang',`unit`='$unit',`harga`='$harga',`jumlah`='$jumlah' where `Id`='$Id'");
+ $q=mysqli_query($con,"UPDATE `pembelian` SET `nama_pembeli`='$nama_pembeli',`nama_barang`='$nama_barang',`unit`='$unit',`harga`='$harga',`jumlah`='$jumlah' where `Id`='$Id'");
  if($q)
  echo "success";
  else
